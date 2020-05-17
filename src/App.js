8778c3b743c842fff2c929a9ad12b42e990ab1cf
sanghuncho <moondrive@hotmail.co.kr>
2020-05-17 06:14:02 +0200

    app menu and table as example are implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ReactDOM from 'react-dom';
-import {Button, AppBar, Toolbar, IconButton, Typography } from '@material-ui/core/';
+import {Button, AppBar, Toolbar, IconButton, Typography, Container  } from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
 import { makeStyles } from '@material-ui/core/styles';
 import ListItem from '@material-ui/core/ListItem';
@@ -9,10 +9,13 @@ import List from '@material-ui/core/List';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
+import BuyingService from './management/BuyingService'
+import ShippingService from './management/ShippingService'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -20,12 +23,21 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+  // content: {
+  //   flexGrow: 1,
+  //   height: '100vh',
+  //   overflow: 'auto',
+  // },
+  // container: {
+  //   paddingTop: theme.spacing(4),
+  //   paddingBottom: theme.spacing(4),
+  // },
 }));
 
 function App() {
   const classes = useStyles();
   return (
-    <div>
+    <div >
     <AppBar position="static">
       <Toolbar>
         <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
@@ -37,9 +49,16 @@ function App() {
         {/* <Button color="inherit">Login</Button> */}
       </Toolbar>
     </AppBar>
-    <PinnedSubheaderList>
+    {/* <div className={classes.root}> */}
     
-    </PinnedSubheaderList>
+    <AppMenu/>
+
+    {/* <main className={classes.content}>
+        <Container maxWidth="lg" className={classes.container}>
+          <BuyingService/>
+        </Container>
+    </main> */}
+    {/* </div> */}
     </div>
   );
 }
@@ -49,13 +68,30 @@ export default App;
 
 const useStylesList = makeStyles((theme) => ({
   root: {
+    display: 'flex',
+  },
+  listMenu: {
     width: '100%',
-    maxWidth: 150,
+    maxWidth: 130,
     backgroundColor: theme.palette.background.paper,
   },
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+  },
+  container: {
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+  },
+  
 }));
 
-function PinnedSubheaderList() {
+function ListItemLink(props) {
+  return <ListItem button component="a" {...props} />;
+}
+
+function AppMenu() {
   const classes = useStylesList();
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
@@ -63,8 +99,16 @@ function PinnedSubheaderList() {
     setSelectedIndex(index);
   };
 
+  //const appMenu = selectedIndex;
+  let content
+  if (selectedIndex == 1) {
+    content = <BuyingService/>
+  } else {
+    content = <ShippingService/>
+  }
   return (
     <div className={classes.root}>
+    <div className={classes.listMenu}>
       <List component="nav" aria-label="shipping service folder">
         <ListItem
           button
@@ -74,9 +118,6 @@ function PinnedSubheaderList() {
           <ListItemText primary="배송대행" />
         </ListItem>
         
-      </List>
-      <Divider />
-      <List component="nav" aria-label="buying service folder">
         <ListItem
           button
           selected={selectedIndex === 2}
@@ -84,9 +125,22 @@ function PinnedSubheaderList() {
         >
           <ListItemText primary="구매대행" />
         </ListItem>
+        <ListItem
+          button
+          selected={selectedIndex === 3}
+          onClick={(event) => handleListItemClick(event, 3)}
+        >
+          <ListItemText primary="Q & A" />
+        </ListItem>
         
       </List>
-
+      </div>
+      <main className={classes.content}>
+        <Container maxWidth="lg" className={classes.container}>
+          {content}
+        </Container>
+      </main>
+     
       </div>
   );
 }
